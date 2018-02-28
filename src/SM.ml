30f5ea8ae7b7fc234c2cd455211def8e52902f0d
Daniyar Itegulov <ditegulov@gmail.com>
2018-02-28 13:23:53 +1100

    Implement compile and eval for stack machine

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,17 @@
-open GT       
-       
+open GT
+open Syntax
+open List
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -22,8 +24,18 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+let eval config prog =
+  let eval' (stack, (state, is, os)) = function
+    | BINOP op ->
+      let y :: x :: xs = stack and f = Expr.parseOperation op
+      in (f x y :: xs, (state, is, os))
+    | CONST c -> (c :: stack, (state, is, os))
+    | READ -> (hd is :: stack, (state, tl is, os))
+    | WRITE -> (tl stack, (state, is, hd stack :: os))
+    | LD name -> (state name :: stack, (state, is, os))
+    | ST name -> (tl stack, (Expr.update name (hd stack) state, is, os))
+  in fold_left eval' config prog
 
 (* Top-level evaluation
 
@@ -33,6 +45,11 @@ let eval _ = failwith "Not yet implemented"
 *)
 let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
 
+let rec compileExpr = function
+  | Expr.Const c -> [CONST c]
+  | Expr.Var name -> [LD name]
+  | Expr.Binop (op, x, y) -> compileExpr x @ compileExpr y @ [BINOP op]
+
 (* Stack machine compiler
 
      val compile : Syntax.Stmt.t -> prg
@@ -40,5 +57,8 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-
-let compile _ = failwith "Not yet implemented"
+let rec compile = function
+  | Stmt.Read var -> [READ; ST var]
+  | Stmt.Write expr -> compileExpr expr @ [WRITE]
+  | Stmt.Assign (var, expr) -> compileExpr expr @ [ST var]
+  | Stmt.Seq (x, y) -> compile x @ compile y
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,14 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,32 +22,64 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
 
+    let intToBool = (<>) 0
+    let boolToInt v = if v then 1 else 0
+
+    let ($$) f g x y = f (g x) (g y)
+
+    let parseOperation op = match op with
+      | "+" -> (+)
+      | "-" -> (-)
+      | "*" -> ( * )
+      | "/" -> (/)
+      | "%" -> (mod)
+      | _   ->
+        let parseBooleanOp = match op with
+          | "==" -> (=)
+          | "!=" -> (<>)
+          | "<=" -> (<=)
+          | "<"  -> (<)
+          | ">=" -> (>=)
+          | ">"  -> (>)
+          | "!!" -> (||) $$ intToBool
+          | "&&" -> (&&) $$ intToBool
+          | _    -> failwith "Unsupported operator"
+        in fun x y -> boolToInt @@ parseBooleanOp x y
+
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let eval st e =
+      let rec eval' st e = match e with
+        | Const c         -> c
+        | Var name        -> st name
+        | Binop(op, x, y) ->
+          let x = eval' st x and y = eval' st y
+          in (parseOperation op) x y
+      in eval' st e
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
+    open List
 
     (* The type for statements *)
     @type t =
@@ -57,7 +89,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -65,14 +97,19 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
+    let rec eval (state, is, os) = function
+      | Read var -> Expr.update var (hd is) state, tl is, os
+      | Write expr -> state, is, (Expr.eval state expr) :: os
+      | Assign (var, expr) -> Expr.update var (Expr.eval state expr) state, is, os
+      | Seq (expr1, expr2) -> eval (eval (state, is, os) expr1) expr2
+
+
   end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
